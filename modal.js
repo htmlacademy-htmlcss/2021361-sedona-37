30b2fb71727a0a5386e30afa34240777f7d0f384
DenisPepper <thech@yandex.ru>
2023-07-11 00:12:20 +0300

    Добавит focus-trap для модалки

---
+++ modal.js
@@ -0,0 +1,13 @@
+let focusholderStart, focusholderEnd, dateFrom;
+
+const elements = document.querySelectorAll('.modal__focusholder--start, .modal__focusholder--end, #date-from');
+for (const element of elements) {
+  element.classList.contains('modal__focusholder--start') && (focusholderStart = element);
+  element.classList.contains('modal__focusholder--end') && (focusholderEnd = element);
+  element.id === 'date-from' && (dateFrom = element);
+}
+
+if (focusholderStart && dateFrom && focusholderEnd) {
+  focusholderStart.addEventListener('focus', () => dateFrom.focus());
+  focusholderEnd.addEventListener('focus', () => dateFrom.focus());
+}
